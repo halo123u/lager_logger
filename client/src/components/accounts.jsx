0af8d46f09983f45245c95f1866d27f3496f8bf6
Julia Graves <jgraves@wesleyan.edu>
2017-08-29 17:26:11 -0400

    ya its working

--- client/src/components/accounts.jsx
+++ client/src/components/accounts.jsx
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class Accounts extends Component {
+	constructor() {
+		super();
+		this.state = {
+			accounts : null,
+			accountsLoaded: false,
+		}
+		this.renderAccount = this.renderAccount.bind(this);
+	}
+
+	componentDidMount() {
+		console.log('did mount');
+		axios.get('/accounts')
+		.then(res => {
+			console.log(res.data);
+			this.setState({
+				accounts: res.data,
+				accountsLoaded: true
+			});
+		}).catch(err => console.log(err));
+	}
+
+	renderAccount(account) {
+		return(
+			<div className='box account'>
+				<h3>{account.company}</h3>
+				<h3>#{account.account_num}</h3>
+			</div>
+		)	
+	}
+
 	render () {
 		return (
 			<div id='accounts'>
@@ -10,12 +41,7 @@ class Accounts extends Component {
 					<h5>Recent Activity</h5>
 					<a>Filter</a>
 				</div>
-
-				<div className='box activity'>
-					<h3>Drexlers</h3>
-					<h3>#RD0956</h3>
-				</div>
-
+				{this.state.accountsLoaded ? this.state.accounts.map(this.renderAccount) : ''}
 			</div>
 		);
 	}
