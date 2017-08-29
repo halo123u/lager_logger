d4e644eb9395f1e89c14dcbb8d2ea396fcc2ce89
Brandon <digihew@gmail.com>
2017-08-28 23:06:11 -0400

    Create updater for orders model

--- models/orders.js
+++ models/orders.js
@@ -1,57 +1,76 @@
 const db = require('../db/config');
 
-const Orders = {
-    create : order => {
-        return db.one(`
-            INSERT INTO orders
-            (refence_number,
-            delivery_info,
-            account_id,
-            order_date,
-            employee_id)
-            VALUES
-            ($1,$2,$3,$4,$5)
-            RETURNING *
-        `, [order.refence_number,
-            order.delivery_info,
-            order.account_id,
-            order.order_date,
-            order.employee_id]);
-    },
+const Orders = {}
+
+Orders.create = order => {
+    return db.one(`
+        INSERT INTO orders
+        (refence_number,
+        delivery_info,
+        account_id,
+        order_date,
+        employee_id)
+        VALUES
+        ($1,$2,$3,$4,$5)
+        RETURNING *
+    `, [order.refence_number,
+        order.delivery_info,
+        order.account_id,
+        order.order_date,
+        order.employee_id]);
+},
 
 
-    // ------------------- Finders --------------------------
-    findByReferenceNumber: refNum => {
-        return db.many(`
-            SELECT * FROM orders
-            WHERE refence_number = $1
-        `,[refNum])
-    },
-    findByAccountId: account_id => {
-        return db.many(`
-            SELECT * FROM orders
-            WHERE account_id = $1
-        `,[account_id])
-    },
-    findByDeliveryInfo: delivery_info => {
-        return db.many(`
-            SELECT * FROM orders
-            WHERE delivery_info = $1
-        `,[delivery_info])
-    },
-    findByOrderDate: order_date => {
-        return db.many(`
-            SELECT * FROM orders
-            WHERE order_date = $1
-        `,[order_date])
-    },
-    findByEmployeeId: employee_id => {
-        return db.many(`
-            SELECT * FROM orders
-            WHERE employee_id = $1
-        `,[employee_id])
-    },
+// ------------------- Finders --------------------------
+Orders.findByReferenceNumber= refNum => {
+    return db.many(`
+        SELECT * FROM orders
+        WHERE refence_number = $1
+    `,[refNum])
+},
+Orders.findByAccountId= account_id => {
+    return db.many(`
+        SELECT * FROM orders
+        WHERE account_id = $1
+    `,[account_id])
+},
+Orders.findByDeliveryInfo= delivery_info => {
+    return db.many(`
+        SELECT * FROM orders
+        WHERE delivery_info = $1
+    `,[delivery_info])
+},
+Orders.findByOrderDate= order_date => {
+    return db.many(`
+        SELECT * FROM orders
+        WHERE order_date = $1
+    `,[order_date])
+},
+Orders.findByEmployeeId= employee_id => {
+    return db.many(`
+        SELECT * FROM orders
+        WHERE employee_id = $1
+    `,[employee_id])
+},
+
+
+
+// ------------------- Updaters --------------------------
+Orders.update = (options) => {
+    return db.one(`
+        UPDATE orders SET
+        refence_number = $1,
+        delivery_info = $2,
+        account_id = $3,
+        order_date = $4,
+        employee_id = $5
+        WHERE order_id = $6
+    `,[options.refence_number,
+        options.delivery_info,
+        options.account_id,
+        options.order_date,
+        options.employee_id,
+        options.order_id])
 }
-
 
 module.exports = User;
