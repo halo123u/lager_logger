1ab69d463587bf2e410b8e44cace376819f35755
Carly Warner <cwarner523>
2017-08-29 18:31:50 -0400

    create visits models

---
+++ models/visits.js
@@ -0,0 +1,39 @@
+const db = require('../db/config');
+
+const Visits = {}
+
+Visits.create = visit => {
+    return db.one(`
+    INSERT INTO visits
+    (date_info,
+    account_id,
+    employee_id)
+    VALUES
+    ($1, $2, $3)
+    `, [visit.date_info,
+        visits.account_id,
+        loyee_id])
+}
+    
+Visits.findByMoment = moments => {
+    return db.query(`
+    SELECT * FROM visits
+    WHERE date_info = $1
+    `, [moment])
+}
+
+Visits.findByAccountId = account_id => {
+    return db.query(`
+    SELECT * FROM visits
+    WHERE account_id = $1
+    `, [account_id])
+}
+
+Visits.findByEmployeeId = employee_id => {
+    return db.query(`
+    SELECT * FROM visits
+    WHERE employee_id = $1
+    `, [account_id])
+}
+
+module.exports = Visits;
