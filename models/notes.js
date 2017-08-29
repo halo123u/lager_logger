0db65f19eceb5620977601077cf7cf8980e7d930
Aury Rodriguez <arm010286@hotmail.com>
2017-08-29 07:05:40 -0400

    Add notes nodel

--- models/notes.js
+++ models/notes.js
@@ -4,27 +4,34 @@ const notes={
         return db.one(`INSERT INTO notes
                     (relationship_id, note_type, content, date_info, employee_id)
                     VALUES ($1, $2, $3, $4, $5) RETURNING * `,
-                    [note.relationship_id, note.note_type, note.content, note.date_info, note.employee_id])
-        }
-
-   update : (note) => {
-        return db.one(`UPDATE notes SET
-                    relationship_id = $1, note_type = $2, content = $3, date_info = $4, employee_id =$5
-                    WHERE id=$6`,
-                    [note.relationship_id, note.note_type, note.content, note.date_info,
-                    note.employee_id, note.id])
-        }
-
-    findById : (id) => {
-        return db.query(`SELECT * FROM notes WHERE id = $1`, [id])
+                    [note.relationship_id, note.note_type, note.content, note.date_info, note.employee_id]
+                )
     }
 
-    findByType = noteId => {
-  return db.query(`SELECT * FROM notes WHERE id = $1`, [noteid])
+    update : (note) => {
+        return db.one(`UPDATE notes SET
+                    relationship_id = $1, note_type = $2, content = $3, date_info = $4, employee_id =$5
+                    WHERE note_id=$6`,
+                    [note.relationship_id, note.note_type, note.content, note.date_info,
+                    note.employee_id, note.note_id]
+                )
+    }
+
+    findById : (note_id) => {
+        return db.manyOrNone(`SELECT n.*, concat(e.first_name , ' ', e.last_name) as employee_name FROM notes n  LEFT JOIN  employees e on n.employee_id = e.emp_id WHERE note_id = $1`, [note_id])
+    },
+
+    findByType : (type) => {
+        return db.query(`SELECT n.*, concat(e.first_name , ' ', e.last_name) as employee_name FROM notes n  LEFT JOIN  employees e on n.employee_id = e.emp_id WHERE note_type = $1`, [type])
+    },
+
+    findByRelationship : (note ) => {
+        return db.query(`SELECT n.*, concat(e.first_name , ' ', e.last_name) as employee_name FROM notes n  LEFT JOIN  employees e on n.employee_id = e.emp_id WHERE note_type= $1 and relationship_id = $2 `, [note.note_type, note.relationship_id])
+    },
+
+    delete : (note_id) => {
+        return db.query(`DELETE FROM notes WHERE note_id = $1`, [note_id])
+    }
 }
 
-notes.delete = noteId => {
-  return db.none(`DELETE FROM notes WHERE id = $1`, [noteId])
-}
-}
 module.exports = notes;
