b1d877b76bd9ed95e67a0e0b49c329ed5d920198
Brandon <digihew@gmail.com>
2017-08-28 22:59:28 -0400

    Create finders for orders

--- models/orders.js
+++ models/orders.js
@@ -11,11 +11,45 @@ const Orders = {
             employee_id)
             VALUES
             ($1,$2,$3,$4,$5)
+            RETURNING *
         `, [order.refence_number,
             order.delivery_info,
             order.account_id,
             order.order_date,
             order.employee_id]);
+    },
+
+
+    // ------------------- Finders --------------------------
+    findByReferenceNumber: refNum => {
+        return db.many(`
+            SELECT * FROM orders
+            WHERE refence_number = $1
+        `,[refNum])
+    },
+    findByAccountId: account_id => {
+        return db.many(`
+            SELECT * FROM orders
+            WHERE account_id = $1
+        `,[account_id])
+    },
+    findByDeliveryInfo: delivery_info => {
+        return db.many(`
+            SELECT * FROM orders
+            WHERE delivery_info = $1
+        `,[delivery_info])
+    },
+    findByOrderDate: order_date => {
+        return db.many(`
+            SELECT * FROM orders
+            WHERE order_date = $1
+        `,[order_date])
+    },
+    findByEmployeeId: employee_id => {
+        return db.many(`
+            SELECT * FROM orders
+            WHERE employee_id = $1
+        `,[employee_id])
     },
 }
 
