e19e3f27e9552d3e786c43c57edadabedb1e18db
Aury Rodriguez <arm010286@hotmail.com>
2017-08-30 15:09:55 -0400

    I dont remember

--- models/notes.js
+++ models/notes.js
@@ -2,26 +2,44 @@ const db = require('../db/config');
 const notes={
     create : (note) => {
         return db.one(`INSERT INTO notes
-                    (relationship_id, note_type, content, date_info, employee_id)
+                    (relationship_id,
+                    note_type,
+                    content,
+                    date_info,
+                    employee_id)
                     VALUES ($1, $2, $3, $4, $5) RETURNING * `,
-                    [note.relationship_id, note.note_type, note.content, note.date_info, note.employee_id]
+                    [note.relationship_id,
+                    note.note_type,
+                    note.content,
+                    note.date_info,
+                    note.employee_id]
                 )
     },
 
     update : (note) => {
         return db.none(`UPDATE notes SET
-                     content = $1, date_info = $2, employee_id =$3
-                      WHERE note_id=$4`,
-                    [ note.content, note.date_info, note.employee_id, note.note_id]
+                    content = $1,
+                    date_info = $2, employee_id =$3
+                    WHERE note_id=$4`,
+                    [note.content,
+                    note.date_info,
+                    note.employee_id,
+                    note.note_id]
                 )
     },
 
     findById : (note_id) => {
-        return db.manyOrNone(`SELECT n.*, concat(e.first_name , ' ', e.last_name) as employee_name FROM notes n  LEFT JOIN  employees e on n.employee_id = e.emp_id WHERE note_id = $1`, [note_id])
+        return db.manyOrNone(`SELECT n.*,
+                              concat(e.first_name , ' ', e.last_name) as employee_name
+                              FROM notes n  LEFT JOIN  employees e on n.employee_id = e.emp_id
+                              WHERE note_id = $1`, [note_id])
     },
 
     findAllByType : (note_type) => {
-        return db.query(`SELECT n.*, concat(e.first_name , ' ', e.last_name) as employee_name FROM notes n  LEFT JOIN  employees e on n.employee_id = e.emp_id WHERE note_type = $1`, [note_type])
+        return db.query(`SELECT n.*,
+                        concat(e.first_name , ' ', e.last_name) as employee_name
+                        FROM notes n  LEFT JOIN  employees e on n.employee_id = e.emp_id
+                        WHERE note_type = $1`, [note_type])
     },
 
     findAllByRelationship : (note) => {
@@ -52,7 +70,11 @@ const notes={
                 join =""
                 columnSelect=""
         }
-        return db.query(`SELECT n.*, concat(e.first_name , ' ', e.last_name) as employee_name, ${columnSelect} FROM notes n ${join} LEFT JOIN  employees e on n.employee_id = e.emp_id WHERE note_type= $1 and relationship_id = $2 `, [note.note_type, note.relationship_id])
+        return db.query(`SELECT n.*,
+                        concat(e.first_name , ' ', e.last_name) as employee_name, ${columnSelect}
+                        FROM notes n ${join} LEFT JOIN  employees e on n.employee_id = e.emp_id
+                        WHERE note_type= $1 and relationship_id = $2 `,
+                        [note.note_type, note.relationship_id])
     },
 
     delete : (note_id) => {
