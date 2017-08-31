23b0bbc1eb4f7def9fdcf4fb2d74ba9e59bda81b
Carly Warner <cwarner523>
2017-08-31 16:12:03 -0400

    update visits controller

--- controllers/visits-controllers.js
+++ controllers/visits-controllers.js
@@ -1,6 +1,14 @@
 const Visits = require('../models/visits');
 
 const visitsController = {
+    getAll: (req, res) => {
+        Visits.findAll().then(data => {
+            console.log(data);
+            res.json(data);
+        }).catch(err => {
+            console.log(err);
+        })
+    },
     addVisit: (req, res) => {
         console.log(req.body)
         Visits.create ({
@@ -11,6 +19,12 @@ const visitsController = {
             console.log(data);
             res.json(data);
         }).catch(err => console.log(err)); 
+    },
+    getById: (req, res) => {
+        Visits.findById(req.params.id).then(data => {
+            console.log(data);
+            res.json(data);
+        }).catch(err => console.log(err));
     },
     getByDate: (req, res) => {
         Visits.findByMoment(req.params.date_info).then(data => {
@@ -27,6 +41,23 @@ const visitsController = {
     getByEmployee: (req, res) => {
         Visits.findByEmployeeId(req.params.employee_id).then(data => {
             console.log(data);
+            res.json(data);
+        }).catch(err => console.log(err));
+    },
+    editVisit: (req, res) => {
+        console.log(req.params.id);
+        Visits.update({
+            date_info: req.body.date_info,
+            account_id:req.body.account_id,
+            employee_id: req.body.employee_id
+        }, req.params.id).then(data => {
+            console.log(data);
+            res.json(data);
+        }).catch(err => { console.log(err) });
+    },
+    deleteVisit: (req, res) => {
+        Visit.delete(req.params.id).then(data => {
+             console.log(data);
             res.json(data);
         }).catch(err => console.log(err));
     },
